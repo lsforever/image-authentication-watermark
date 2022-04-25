0f62f5f537bb15278481f4704074002ca0077783
Zworld <92575518+zworldinc@users.noreply.github.com>
2022-04-25 14:25:31 +0530

    m

---
+++ setup.py
@@ -0,0 +1,13 @@
+# # setup.py
+
+# from distutils.core import setup
+
+# setup(name='Distutils',
+#       version='1.0',
+#       description='Python Distribution Utilities',
+#       author='kkkkkk',
+#       author_email='kkkk@kkk.kkk',
+#       url='https://www.kkkk.kkk/kkk/',
+#       packages=['distutils', 'distutils.command'],
+      
+#      )
